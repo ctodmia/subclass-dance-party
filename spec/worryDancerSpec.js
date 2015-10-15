aec77ceec6f26af5ab210d041874e87bec30b68d
ctodmia <ctodmia@gmail.com>
2015-10-14 20:01:23 -0700

    added new dancer

---
+++ spec/worryDancerSpec.js
@@ -0,0 +1,35 @@
+describe("worryDancer", function() {
+
+  var worryDancer;
+  var timeBetweenSteps = 100;
+  var clock;
+
+  beforeEach(function() {
+    clock = sinon.useFakeTimers();
+    worryDancer = new makeWorryDancer(10, 20, timeBetweenSteps);
+  });
+
+  it("should have a jQuery $node object", function(){
+    expect(worryDancer.$node).to.be.an.instanceof(jQuery);
+  });
+
+  it("should have a step function that makes its node change colors", function() {
+    sinon.spy(worryDancer.$node, 'css');
+    worryDancer.step();
+    expect(worryDancer.$node.css.called).to.be.true;
+  });
+
+  describe("dance", function(){
+    it("should call step at least once per second", function(){
+      sinon.spy(worryDancer, "step");
+      expect(worryDancer.step.callCount).to.be.equal(0);
+      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
+      clock.tick(timeBetweenSteps);
+
+      expect(worryDancer.step.callCount).to.be.equal(1);
+
+      clock.tick(timeBetweenSteps);
+      expect(worryDancer.step.callCount).to.be.equal(2);
+    });
+  });
+});
--- src/worryDancer.js
+++ src/worryDancer.js
@@ -1,15 +1,15 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps){
+var makeWorryDancer = function(top, left, timeBetweenSteps){
   makeDancer.call(this, top, left, timeBetweenSteps);
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
-
+  this.$node = $('<span class="dancer"></span>');
   this.oldStep = this.step;
 };
 
-makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
-makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
-makeBlinkyDancer.prototype.step = function (){
+makeWorryDancer.prototype = Object.create(makeDancer.prototype);
+makeWorryDancer.prototype.constructor = makeWorryDancer;
+makeWorryDancer.prototype.step = function (){
 
       //call the old version of step at the beginning of any call to this new version of step
       //there is an old function of step that will be invocated on the prototype of our superclass.
@@ -17,11 +17,12 @@ makeBlinkyDancer.prototype.step = function (){
       //toggle() is a jQuery method to show/hide the <span> tag.
      // See http://api.jquery.com/category/effects/ for this and
      // other effects you can use on a jQuery-wrapped html tag.
+    
+    this.$node.css("color","blue");
     this.$node.toggle();
     setTimeout(this.step.bind(this), this.timeBetweenSteps);
 };
 
-makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
+makeWorryDancer.prototype.constructor = makeWorryDancer;
 
 // var blinkyDancer = new makeBlinkyDancer(20, 10, 4000);
-
